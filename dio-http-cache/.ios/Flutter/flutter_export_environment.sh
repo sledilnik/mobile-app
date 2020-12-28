ef597c255067ba6a4da76217aba5c3262d28c33a
Miha Markič <miha@rthand.com>
2020-12-28 12:11:20 +0100

    Adds missing dio_http_cache files

---
+++ dio-http-cache/.ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/miha/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/miha/Projects/sledilnik/mobile-app/dio-http-cache"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_BUILD_NAME=0.2.11"
+export "FLUTTER_BUILD_NUMBER=0.2.11"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
